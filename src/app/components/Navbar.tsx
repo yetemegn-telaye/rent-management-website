7af43522417bf7c876efdae4cdf702b74fa48f42
yetemegn-telaye <lemma19yet@gmail.com>
2024-08-22 16:39:03 +0300

    Fix navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,12 +5,12 @@ export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false); // State to manage menu toggle
 
     return (
-        <div className="relative top-4 mx-2 p-3 px-8 flex justify-between items-center bg-white rounded-full text-dark">
+        <div className="relative top-4 mx-2 p-3 px-12 flex justify-between items-center bg-white rounded-full text-dark">
             <div className="font-semibold text-lg">
                 <h1>Ambassador Mall</h1>
                 <p className="text-gray-400 font-light text-sm">XP Property Management</p>
             </div>
-            <div className={`${isOpen ? 'flex' : 'hidden'} flex-col text-sm lg:flex lg:items-center lg:justify-center absolute lg:relative top-full lg:top-0 right-0 lg:right-auto bg-white lg:bg-transparent z-20 w-full lg:w-auto`}>
+            <div className={`${isOpen ? 'flex' : 'hidden'} flex-col rounded-half text-sm lg:flex lg:items-center lg:justify-center absolute lg:relative top-full lg:top-0 right-0 lg:right-auto bg-white lg:bg-transparent z-20 w-full lg:w-auto`}>
                 <ul className="flex flex-col lg:flex-row lg:items-center gap-4 p-4 lg:p-0">
                     <li className="text-black hover:text-blue-200 cursor-pointer">
                         <Link href="/">Home</Link>
@@ -29,8 +29,8 @@ export default function Navbar() {
                     </li>
                 </ul>
             </div>
-            <div className="flex flex-col lg:flex-row gap-4 p-4 lg:p-0 text-sm">
-                <p className="text-black">
+            <div className="flex gap-4 p-4 lg:p-0 text-sm">
+                <p>
                     <span className="mr-2">📞</span>+25193423112
                 </p>
                 <p className="font-light">sign in</p>
